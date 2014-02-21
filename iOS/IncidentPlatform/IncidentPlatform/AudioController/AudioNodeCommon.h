39ac23f4ec0c708ce7a37a0abf9b3e640ffa5925
Idan Beck <idanbeck@incidenttech.com>
2014-02-20 17:32:09 -0800

    Adding the phantom AudioNodeCommon.h

---
+++ iOS/IncidentPlatform/IncidentPlatform/AudioController/AudioNodeCommon.h
@@ -0,0 +1,22 @@
+//
+//  AudioNodeCommon.h
+//  IncidentPlatform
+//
+//  Created by Idan Beck on 2/13/14.
+//  Copyright (c) 2014 Incident Technologies, Inc. All rights reserved.
+//
+
+#ifndef IncidentPlatform_AudioNodeCommon_h
+#define IncidentPlatform_AudioNodeCommon_h
+
+#include "AudioNode.h"
+#include "WavetableNode.h"
+#include "GeneratorNode.h"
+#include "EnvelopeNode.h"
+#include "SampleNode.h"
+#include "EffectNode.h"
+#include "DelayNode.h"
+#include "AudioNodeNetwork.h"
+#include "SamplerNode.h"
+
+#endif
